c6377300a920687865a5d240909c741b3693135d
Subham Dikshit <subhamdikshit007@gmail.com>
2023-07-18 16:18:49 +0530

    frontend fonts change

--- frontend/src/shared/Newsletter.jsx
+++ frontend/src/shared/Newsletter.jsx
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 
 
 
-import { React, useRef ,useState} from "react";
+
+
+
+              import { React, useRef ,useState} from "react";
 import "./newsletter.css";
 import { Container, Row, Col } from "reactstrap";
-=======
-import { React, useRef ,useState} from "react";
-import "./newsletter.css";
-import { Container, Row, Col } from "reactstrap";
-import maleTourist from "../assets/images/male-tourist.png";
->>>>>>> e9ca718161b804a949ad61d561c55f5120db0d7d
 import emailjs from "@emailjs/browser";
 
 
@@ -43,15 +39,9 @@ const NewsLetter = () => {
     <section className="newsletter">
       <Container>
         <Row>
-<<<<<<< HEAD
-          <Col >
+          <Col>
             <div className="newsletter__content">
               <h2>SUBSCRIBE NOW</h2>
-=======
-          <Col lg="6">
-            <div className="newsletter__content">
-              <h2>Subcribe now to get useful traveling information</h2>
->>>>>>> e9ca718161b804a949ad61d561c55f5120db0d7d
 
               <form ref={ref} onSubmit={handleSubmit}>
                 <div className="newsletter__input">
@@ -62,12 +52,12 @@ const NewsLetter = () => {
                   <button  type ="submit"className="btn newsletter__btn">Subcribe</button>
                 </div>
                 {success &&
-<<<<<<< HEAD
-              <div>Your are Subscribed Now. We'll get back to you soon</div> }
+                <div>Your are Subscribed Now. We'll get back to you soon</div>
+                }
  
               </form>
               <p>
-                Are you passionate about travel, adventure, and discovering new
+              Are you passionate about travel, adventure, and discovering new
                 destinations? 🌍✈️ Don't miss out on the latest updates,
                 exclusive travel tips, and special offers that will make your
                 wanderlust heart skip a beat! Subscribe to our newsletter now
@@ -75,31 +65,10 @@ const NewsLetter = () => {
               </p>
             </div>
           </Col>
-
-=======
-              ("Your are Subscribed Now. We'll get back to you soon :)")}
- 
-              </form>
-              <p>
-                {/* Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati adipisici sunt in, provident facere ipsam? */}
-              </p>
-            </div>
-          </Col>
-          <Col lg="6">
-            <div className="newsletter__img">
-              <img src={maleTourist} alt="" />
-            </div>
-          </Col>
->>>>>>> e9ca718161b804a949ad61d561c55f5120db0d7d
         </Row>
       </Container>
     </section>
   );
 };
 
-<<<<<<< HEAD
 export default NewsLetter;
-=======
-export default NewsLetter;
->>>>>>> e9ca718161b804a949ad61d561c55f5120db0d7d
